bd0c86b25ab6a1f412fc2bf5d4f1eb2a2bb0ce78
Mike Peachey <mike.peachey@bjss.com>
2016-08-11 21:38:14 +0100

    more hell because of lack of JIT interpolation for the count graph

--- nat_gateways.tf
+++ nat_gateways.tf
@@ -1,10 +1,10 @@
 resource "aws_eip" "eips" {
-  count = "${length(module.subnets.subnet_ids)}"
+  count = "${length(var.subnets_cidr)}"
   vpc   = "true"
 }
 
 resource "aws_nat_gateway" "nat_gateways" {
-  count         = "${length(module.subnets.subnet_ids)}"
+  count         = "${length(var.subnets_cidr)}"
   allocation_id = "${element(aws_eip.eips.*.id, count.index)}"
   subnet_id     = "${module.subnets.subnet_ids[count.index]}"
 }
